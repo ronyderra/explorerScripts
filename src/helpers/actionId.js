17db94403e64d94b3cdfcb74c058fa6cdb2619d5
ronyXnes <derrarony@gmail.com>
2022-08-01 17:38:12 +0300

    elegantUnpair elegantPair

---
+++ src/helpers/actionId.js
@@ -0,0 +1,10 @@
+export function elegantUnpair(z) {
+    var sqrtz = Math.floor(Math.sqrt(z)),
+      sqz = sqrtz * sqrtz;
+    return z - sqz >= sqrtz ? [sqrtz, z - sqz - sqrtz] : [z - sqz, sqrtz];
+  }
+  
+  export function elegantPair(x, y) {
+    return x >= y ? x * x + x + y : y * y + x;
+  }
+  
